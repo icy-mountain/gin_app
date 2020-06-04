cb9db3789cd217566a1fff302c4aeafd3b28c703
icy-mountain <ym.1211@icloud.com>
2020-06-04 18:47:32 +0900

    bootstrap

--- test/main.go
+++ test/main.go
@@ -8,16 +8,17 @@ import (
 )
 
 func main() {
-	host := flag.String("host", ":8080", "The host of the application.")
+	port := flag.String("port", ":8080", "The host of the application.")
+	flag.Parse()
 	r := gin.Default()
 	room := newRoom()
 	go room.run()
 	r.LoadHTMLGlob("templates/*.html")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "chat.html", gin.H{
-			"Host": "localhost" + *host,
+			"Host": c.Request.Host,
 		})
 	})
 	r.GET("/room", room.ServeHTTP)
-	r.Run(*host) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(*port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
